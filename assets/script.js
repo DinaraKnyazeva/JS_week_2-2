c095e3da8c4b48663d67796eefa1414d11ff017f
DinaraKnyazeva <Dinarka470@mail.ru>
2023-03-24 22:11:55 +0400

    comments for myself

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,19 @@
+// мин.кол-во картинок
 let currentImage = 1;
+// макс.кол-во картинок
 let totalImages = 8;
 
 function nextSlides() {
+  // например: один клик на право получается 1+1
   currentImage++;
+  // проверка: если 3 не больше 8 то дальше (чтобы было как "карусель")
   if (currentImage > totalImages) {
     currentImage = 1;
   }
+  // здесь src путь склеивается со значение 3 (т.е. картинка №3)
   document.getElementById("img").src = "images/cat" + currentImage + ".jpg";
 }
-
+// то же самое только в обратную сторону
 function prevSlides() {
   currentImage--;
   if (currentImage < 1) {
